6247ec50c8240a11f17c64cfcfd0e366cdd7bc0f
wxia <wxia@dfocuspace.com>
2020-04-25 16:06:58 +0800

    fix: 添加基础的webpack配置

--- build/webpack-dev.config.js
+++ build/webpack-dev.config.js
@@ -1,68 +1,9 @@
-const path = require('path')
-// const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpackMerge = require('webpack-merge')
+const webpackBaseConfig = require('./webpack-base.config')
 const dev = require('./min-webpack/dev')
 
-const webpackConfig = {
-  mode: 'development',
-  entry: {
-    app: path.resolve(__dirname, '../src/index.tsx')
-  },
-  // output: {
-  //   filename: '[name].[hash:8].js',
-  //   path: path.resolve(__dirname, '../dist')
-  // },
-  // stats: 'errors-only',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        include: [path.resolve(__dirname, '../src')],
-        use: ['babel-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: 'style-loader' // creates style nodes from JS strings
-          },
-          {
-            loader: 'css-loader' // translates CSS into CommonJS
-          },
-          {
-            loader: 'less-loader' // compiles Less to CSS
-          }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader'
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx'],
-    modules: [path.resolve(__dirname, '../node_modules'), 'node_modules']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-  ]
-}
+const webpackConfig = webpackMerge(webpackBaseConfig, {
+  mode: 'development'
+})
 
 dev({ webpackConfig })
--- build/webpack-prod.config.js
+++ build/webpack-prod.config.js
@@ -1,8 +1,9 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpackMerge = require('webpack-merge')
+const webpackBaseConfig = require('./webpack-base.config')
 const build = require('./min-webpack/build')
 
-const webpackConfig = {
+const webpackConfig = webpackMerge(webpackBaseConfig, {
   mode: 'production',
   entry: {
     app: './src/index.tsx'
@@ -10,53 +11,7 @@ const webpackConfig = {
   output: {
     filename: '[name].[hash:8].js',
     path: path.resolve(__dirname, '../dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: 'style-loader' // creates style nodes from JS strings
-          },
-          {
-            loader: 'css-loader' // translates CSS into CommonJS
-          },
-          {
-            loader: 'less-loader' // compiles Less to CSS
-          }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader'
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-  ]
-}
+  }
+})
 
 build({ webpackConfig })
